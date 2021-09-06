cc4603de82dd8c608a9b061d887ff2e5c87586e2
Jesse Vermeulen <jessevermeulen123@gmail.com>
2021-09-06 16:03:01 +0200

    authors

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,23 +1,22 @@
 // bin/seeds.js
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const books = require("./books-data")
-
-const Book = require('../models/Book.model');
+const books = require("./book-data");
+const Book = require("../models/Book.model");
 
 // require database configuration
-require('../configs/db.config');
+require("../configs/db.config");
 
 Book.deleteMany()
-.then(deletedBooks =>
-  console.log(`Deleted ${deletedBooks.deletedCount} books`)
-)
-.then(
-  Book.insertMany(books)
-  .then(insertedBooks => {
-    console.log(`Created ${insertedBooks.length} books`)
-    mongoose.connection.close()}
-  ))
-.catch(err =>
-   console.log(`An error occurred seeding books to the DB: ${err}`)
-)
+  .then((deletedBooks) =>
+    console.log(`Deleted ${deletedBooks.deletedCount} books`)
+  )
+  .then(() => {
+    Book.insertMany(books).then((insertedBooks) => {
+      console.log(`Created ${insertedBooks.length} books`);
+      mongoose.connection.close();
+    });
+  })
+  .catch((err) =>
+    console.log(`An error occurred seeding books to the DB: ${err}`)
+  );
